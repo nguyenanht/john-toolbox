01e7a17453d0e41e01eb99ce6fff12ac218c57fd
johnathan nguyen <nonosnguyen@gmail.com>
2021-03-27 15:19:46 +0100

    feat: add script to retrieve version package

--- john_toolbox/__init__.py
+++ john_toolbox/__init__.py
@@ -1,11 +1,3 @@
-import toml
-from pathlib import Path
+from importlib_metadata import version
 
-
-def get_version():
-    path = Path(__file__).resolve().parents[1] / 'pyproject.toml'
-    pyproject = toml.loads(open(str(path)).read())
-    return pyproject['tool']['poetry']['version']
-
-
-__version__ = get_version()
+__version__ = version(__package__)
