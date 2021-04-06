02c3b6b1fae1d121e52b36d90b2c0d8b8b338518
johnathan nguyen <nonosnguyen@gmail.com>
2021-04-06 21:04:12 +0200

    feat: add version in __init__.py

--- john_toolbox/__init__.py
+++ john_toolbox/__init__.py
@@ -1,3 +1,9 @@
 """
 API reference documentation for the example `john_toolbox` package.
 """
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+
+__version__ = importlib_metadata.version(__name__)
