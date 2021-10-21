f0187ad4e8110971be2e84394fab9ae5b49a9290
johnathan nguyen <nonosnguyen@gmail.com>
2021-10-21 10:43:33 +0200

    chore: add titanic dataset

--- john_toolbox/utils/logger_config.py
+++ john_toolbox/utils/logger_config.py
@@ -7,43 +7,6 @@ import logging.config
 import os
 import json
 
-#
-# FORMATTER = logging.Formatter(
-#     "%(levelname)s - %(asctime)s|%(name)s - %(funcName)s:%(lineno)d — %(message)s",
-#     "%H:%M:%S",
-# )
-#
-# PACKAGE_ROOT = pathlib.Path(__file__).resolve().parent.parent
-#
-# loggers = {}
-#
-#
-# def get_console_handler():
-#     console_handler = logging.StreamHandler(sys.stdout)
-#     console_handler.setFormatter(FORMATTER)
-#     return console_handler
-#
-#
-# def get_logger(*, logger_name):
-#     """Get logger with prepared handlers."""
-#     global loggers
-#
-#     if loggers.get(logger_name):
-#         return loggers.get(logger_name)
-#     else:
-#         logger = logging.getLogger(logger_name)
-#
-#         # logger.setLevel(logging.INFO)
-#         logger.setLevel(logging.INFO)
-#         if logger.hasHandlers():
-#             logger.handlers.clear()
-#
-#         logger.addHandler(get_console_handler())
-#         logger.propagate = False
-#         loggers[logger_name] = logger
-#
-#     return loggers[logger_name]
-
 
 def setup_log_config(is_dev=True):
     """Setup logging configuration.
