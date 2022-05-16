21cfa47ac3a73d8b8b61d10e738c3195a2cb1140
johnathan nguyen <nonosnguyen@gmail.com>
2022-05-16 16:15:46 +0200

    chore: add logger to pandas_transformers

--- john_toolbox/preprocessing/utils.py
+++ john_toolbox/preprocessing/utils.py
@@ -3,6 +3,9 @@ import multiprocessing
 from joblib import Parallel, delayed
 from tqdm import tqdm
 import numpy as np
+import logging
+
+LOGGER = logging.getLogger(__name__)
 
 
 def compute_in_parallel(series, func, **kwargs):
@@ -15,7 +18,7 @@ def compute_in_parallel(series, func, **kwargs):
 
 def get_idx_cat_columns(X, n_modality_cat=15):
     # extract categorical columns
-    cat_cols = X.select_dtypes(include=['object', 'category']).columns.tolist()
+    cat_cols = X.select_dtypes(include=["object", "category"]).columns.tolist()
 
     # extract numerical_columns
     numerical_cols = X.select_dtypes(include=np.number).columns.tolist()
@@ -24,7 +27,9 @@ def get_idx_cat_columns(X, n_modality_cat=15):
     for col in numerical_cols:
 
         if X[col].nunique() <= n_modality_cat:
-            print(f"col = {col} interpreted as categorical, nunique = {X[col].nunique()}")
+            LOGGER.warning(
+                f"numerical col = {col} interpreted as categorical, nunique = {X[col].nunique()}"
+            )
             cat_cols.append(col)
 
     idx_cols = [X.columns.get_loc(col) for col in cat_cols]
@@ -33,7 +38,6 @@ def get_idx_cat_columns(X, n_modality_cat=15):
     cat_cols = np.array(cat_cols)[idx_sorted].tolist()
     idx_cols = np.array(idx_cols)[idx_sorted].tolist()
 
-    return {
-        "cat_cols": cat_cols,
-        "idx_cols": idx_cols
-    }
+    idx_cols_mapping = {"cat_cols": cat_cols, "idx_cols": idx_cols}
+    LOGGER.info(f"idx_cols_mapping = {idx_cols_mapping}.")
+    return idx_cols_mapping
