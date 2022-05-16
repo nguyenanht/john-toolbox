7f9dd32cdf6712e3b1acf5a347bf9d957a47f933
johnathan nguyen <nonosnguyen@gmail.com>
2022-05-16 14:24:14 +0200

    chore: add level params to logger_config

--- john_toolbox/utils/logger_config.py
+++ john_toolbox/utils/logger_config.py
@@ -8,7 +8,16 @@ import os
 import json
 
 
-def setup_log_config(is_dev=False):
+ACCEPTED_LEVEL_MODE = {
+    "INFO": logging.INFO,
+    "DEBUG": logging.DEBUG,
+    "CRITICAL": logging.CRITICAL,
+    "WARNING": logging.WARNING,
+    "ERROR": logging.ERROR,
+}
+
+
+def setup_log_config(is_dev=False, level=None):
     """Setup logging configuration.
 
     Parameters
@@ -17,6 +26,8 @@ def setup_log_config(is_dev=False):
         if True, the dev configuration log file is used (more readable for developers)
         you can activate the dev mode passing 'True' to ENGINE_LOGS_DEV into
         the 'engine-service.yml' from brain_docker
+    level: str, default=None
+        parameters to override
 
     """
     if is_dev:
@@ -26,8 +37,26 @@ def setup_log_config(is_dev=False):
     if os.path.exists(path):
         with open(path, "rt") as f:
             config = json.load(f)
+
+        if level is not None:
+            if level.upper() in ACCEPTED_LEVEL_MODE.keys():
+
+                config["root"]["level"] = level.upper()
+                config["handlers"]["console"]["level"] = level.upper()
+        print(f"level logging = {config['root']['level']}")
         logging.config.dictConfig(config)
     else:
-        logging.basicConfig(level=logging.INFO)
 
+        if level is not None:
+            custom_level = ACCEPTED_LEVEL_MODE.get(level.upper())
+            if custom_level is not None:
+                logging.basicConfig(level=custom_level)
+                print(f"level logging = {level.upper()}")
+            else:
+                logging.basicConfig(level=logging.INFO)
+                print(f"level logging = INFO")
+
+        else:
+            logging.basicConfig(level=logging.INFO)
+            print(f"level logging = INFO")
     return
