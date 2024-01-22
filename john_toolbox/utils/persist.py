c878edf581a853f0b4306b4e5ca81ef4efdf817d
johnathan nguyen <nonosnguyen@gmail.com>
2024-01-22 16:01:10 +0100

    feat: add transformers architecture

--- john_toolbox/utils/persist.py
+++ john_toolbox/utils/persist.py
@@ -1,6 +1,12 @@
 import json
+import logging
+import os
 import pickle
+import shutil
+
 import yaml
+
+logger = logging.getLogger(__name__)
 
 
 def save_pickle(obj, path):
@@ -12,6 +18,23 @@ def load_pickle(path):
     with open(path, "rb") as handle:
         obj = pickle.load(handle)
     return obj
+
+
+def remove_pickle(file_path):
+    if os.path.exists(file_path):
+        # removing the file using the os.remove() method
+        os.remove(file_path)
+    else:
+        # file not found message
+        print("File not found in the directory.")
+
+
+def remove_folder(path):
+    try:
+        shutil.rmtree(path)
+        print("Directory removed successfully")
+    except OSError as o:
+        print(f"Error, {o.strerror}: {path}")
 
 
 def save_json(obj, path):
@@ -34,3 +57,23 @@ def load_yaml(path):
     with open(path, "r") as stream:
         obj = yaml.load(stream, Loader=yaml.SafeLoader)
     return obj
+
+
+def pretty_json(obj: dict):
+    return json.dumps(obj, sort_keys=False, indent=4, separators=(",", ": "))
+
+
+def is_folder_exist(folder_path: str):
+    return os.path.isdir(folder_path)
+
+
+def list_dir(folder_path: str):
+    return os.listdir(folder_path)
+
+
+def create_local_folder(local_dirs: str):
+    try:
+        os.makedirs(local_dirs)
+        logger.debug(f"local_dirs {local_dirs} created successfully")
+    except FileExistsError:
+        logger.debug("no need to create folders as it already exists.")
